6f93a8bf0796663f3151203d27f4f1d80fe1432c
LenovoX-240 <vicclem5999@live.fr>
2018-07-04 22:34:33 +0200

    ajout parallelisation (test_bootstrap_variable) nb_train (monte carlo test bootstrap all) type de donnee (MAP, ilr, comptage)

--- Rscript/classification.R
+++ Rscript/classification.R
@@ -1,21 +1,23 @@
 library(randomForest)
 library(doParallel)
+library(dplyr)
 
 source("Rscript/bootstrap.R")
 
 
 
-create_data_frame_test <- function(data1, apprent, proportion_real_data=0.1){
+create_data_frame_test <- function(data1, apprent, proportion_real_data=0.1, type){
   
   
   rand_real_data <- sample(1:nrow(data1), floor(nrow(data1)*proportion_real_data))
   
-  boot <- simulation(apprent)
+  boot <- simulation(apprent,type = type)
+
   data_train <- rbind(data1, boot)
   metadata <- c(rep("real", nrow(data1)), rep("simu", nrow(boot))) %>% as.factor()
   
   nb_sample <- 1000
-  boot_test <- simulation(apprent, nb_sample = nb_sample)
+  boot_test <- simulation(apprent, nb_sample = nb_sample, type = type)
   
   train <- data.frame(data_train, metadata ,row.names = NULL)
   deleted_data_train <- train[rand_real_data, 1:ncol(data_train)]
@@ -35,14 +37,16 @@ create_data_frame_test <- function(data1, apprent, proportion_real_data=0.1){
 
 
 
-test_bootstrap_all <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nrow(data1), apprent=NULL, nb_train=1){
+test_bootstrap_all <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nrow(data1), apprent=NULL, nb_train=1, type="comptage"){
   
   if(is.null(apprent)) apprent <- apprentissage(data1, nb_axe = nb_axe, nb_cluster = nb_cluster)
   
+  if(type=="ilr") data1 <- data1 %>% MAP() %>% ilr()
   
+  if(type=="MAP") data1 <- data1 %>% MAP()
+    
   all <- lapply(1:nb_train, function(x){
-                            data_test <- create_data_frame_test(data1, apprent=apprent, proportion_real_data = 0.1)
-  
+                            data_test <- create_data_frame_test(data1, apprent=apprent, proportion_real_data = 0.1, type=type)
                             rf <- randomForest(metadata~. , data_test$train)
                             pred <- predict(rf, data_test$test) %>% table(data_test$metadata_test)
                             pred
@@ -60,19 +64,38 @@ test_bootstrap_all <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nr
 
 
 
-test_bootstrap_variable <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nrow(data1), apprent=NULL, nb_train=1){
+test_bootstrap_variable <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nrow(data1), apprent=NULL, nb_train=1, type="comptage"){
   
   
   if(is.null(apprent)) apprent <- apprentissage(data1, nb_axe = nb_axe, nb_cluster = nb_cluster)
   
-  data_test <- create_data_frame_test(data1, apprent=apprent, proportion_real_data = 0.1)
+  if(type=="ilr") data1 <- data1 %>% MAP() %>% center_scale(scale=FALSE) %>% ilr()
+  
+  if(type=="MAP") data1 <- data1 %>% MAP()
+  
+  data_test <- create_data_frame_test(data1, apprent=apprent, proportion_real_data = 0.1, type=type)
   
   variables <- colnames(data_test$test)
   
-  each <- lapply(variables, function(x){
-    rf <- randomForest(as.formula(paste("metadata~",x)), data_test$train)
-    predict(rf, data_test$test) %>% table(data_test$metadata_test)
-  })
+  
+  no_cores <- detectCores()-1
+  if(no_cores>1){
+    
+    cl <- makeCluster(no_cores)
+    clusterEvalQ(cl, {library(randomForest); library(dplyr)})
+      each <- parLapply(cl, variables, function(x){
+                                              rf <- randomForest(as.formula(paste("metadata~",x)), data_test$train)
+                                              predict(rf, data_test$test) %>% table(data_test$metadata_test)
+                                          })
+      
+    stopCluster(cl)
+    
+  }else{
+    each <- lapply(variables, function(x){
+                                      rf <- randomForest(as.formula(paste("metadata~",x)), data_test$train)
+                                      predict(rf, data_test$test) %>% table(data_test$metadata_test)
+                                       })
+  }
   
   misclassification_each <- sapply(each, function(x){x[1, 2]})
   
@@ -86,13 +109,13 @@ test_bootstrap_variable <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_samp
 
 
 
-test_bootstrap <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nrow(data1)){
+test_bootstrap <- function(data1, nb_cluster=NULL, nb_axe=NULL, nb_sample=nrow(data1), type="comptage"){
   
   rand_real_data <- sample(1:nrow(data1), floor(nrow(data1)*0.10))
   
   apprent <- apprentissage(data1, nb_cluster = nb_cluster, nb_axe = nb_axe)
-  all <- test_bootstrap_all(data1, apprent = apprent)
-  each <- test_bootstrap_variable(data1, apprent = apprent)
+  all <- test_bootstrap_all(data1, apprent = apprent, type=type)
+  each <- test_bootstrap_variable(data1, apprent = apprent, type=type)
   
   list(all=all, table_each=each$table_each, misclassification=each$misclassification)
 }
