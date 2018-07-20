57f20c77216091c98ba8f07fa06bf70ce53eb86c
LenovoX-240 <vicclem5999@live.fr>
2018-07-20 22:41:28 +0200

    function: -find_group: predit le groupe des donnees (renvoie les tables de contingence)
    -find_real: trouve les donnees simulees ressemblant au reel (renvoie leur indices dans le vecteur passe en argument)
    
    -find_nb_sample_cluster: calcul le nombre d'echantillon par cluster (metadata)
    
    -classificateur_group_real_simu: trouve les groupes des donnees simule (qui ressemblent au donnees reels)
    
    -modification fonction simulation supervise:
    nombre d'echantillon simule par cluster

--- Rscript/apprentissage_supervise.R
+++ Rscript/apprentissage_supervise.R
@@ -9,32 +9,43 @@ apprentissage_supervise <- function(data, metadata){
   name_group <- unique(metadata)
   apprent <- lapply(name_group, function(x){
     iid <- which(metadata==x)
-    apprentissage(data[iid,])
+    a <- apprentissage(data[iid,])
+    a$metadata <- x
+    class(a) <- "apprentissage supervise"
+    a
   })
   apprent
 }
 
 
-simulation_supervise <- function(apprent){
+simulation_supervise <- function(apprent, nb_sample=NULL){
   data <- NULL
   metadata <- NULL
+  if(length(nb_sample)!=length(unique(apprent)) || any(nb_sample<0)){
+    nb_sample <- NULL
+  }
   for(i in 1:length(apprent)){
-    data <- rbind(data, simulation(apprent[[i]]))
-    metadata <- c(metadata, rep(i, nrow(apprent[[i]]$data)))
+    if(is.null(nb_sample)){
+      data <- rbind(data, simulation(apprent[[i]]))
+      metadata <- c(metadata, rep(i, nrow(apprent[[i]]$data)))
+    }else{
+      data <- rbind(data, simulation(apprent[[i]], nb_sample = nb_sample[i]))
+      metadata <- c(metadata, rep(i, nb_sample[i]))
+    }
+    
   }
   
   list(data=data, metadata=metadata)
 }
 
 
-bootstrap_supervise <- function(data, metadata){
+bootstrap_supervise <- function(data, metadata, nb_sample=NULL){
   apprent <- apprentissage_supervise(data=data, metadata = metadata)
-  simulation_supervise(apprent)
+  simulation_supervise(apprent, nb_sample = nb_sample)
 }
 
 
-
-test_bootstrap_supervise <- function(data, metadata, nb_train=1, type="comptage"){
+test_bootstrap_supervise <- function(data, metadata, nb_train=1, type="comptage", nb_sample=NULL){
   
   apprent <- apprentissage_supervise(data=data, metadata = metadata)
   
@@ -46,7 +57,7 @@ test_bootstrap_supervise <- function(data, metadata, nb_train=1, type="comptage"
   
   l <- lapply(1:nb_train, function(x){
     
-    simu <- simulation_supervise(apprent)
+    simu <- simulation_supervise(apprent, nb_sample = nb_sample)
     
     if(type=="MAP") simu$data <- simu$data %>% MAP()
     if(type=="ilr") simu$data <- simu$data %>% MAP() %>% ilr()
@@ -54,20 +65,9 @@ test_bootstrap_supervise <- function(data, metadata, nb_train=1, type="comptage"
     test <- data.frame(simu$data, row.names = NULL)
     colnames(test) <- paste("X",1:ncol(test), sep="")
     
-    
-    ### random forest
-    forest <- randomForest(metadata~., train)
-    pred_forest <- predict(forest, test)
-    
-    ### kNN
-    kNN <- knn(data, simu$data, cl=metadata, k=1)
-    
-    ### Svm
-    Svm <- svm(metadata~., train)
-    pred_svm <- predict(Svm, test)
-    
-    list(random_forest=table(pred_forest, simu$metadata), kNN=table(kNN, simu$metadata), Svm=table(pred_svm, simu$metadata))
-  })
+    find_group(train, test, simu$metadata)
+      
+   })
   
   
   sum_table <- list(random_forest=0, kNN=0, Svm=0)
@@ -80,6 +80,24 @@ test_bootstrap_supervise <- function(data, metadata, nb_train=1, type="comptage"
   
   list(all=sum_table, all_train=l)
 }
+
+
+find_group <- function(train, test, metadata_test){
+  
+  ### random forest
+  forest <- randomForest(metadata~., train)
+  pred_forest <- predict(forest, test)
+  
+  ### kNN
+  kNN <- knn(train[, -ncol(train)], test, cl=train[, ncol(train)], k=1)
+  
+  ### Svm
+  Svm <- svm(metadata~., train)
+  pred_svm <- predict(Svm, test)
+  
+  list(random_forest=table(pred_forest, metadata_test), kNN=table(kNN, metadata_test), Svm=table(pred_svm, metadata_test))
+}
+
 
 
 
@@ -136,5 +154,60 @@ validation_croise <- function(data, metadata, k=nrow(data)){
   }
   
   list(kNN=table_group_kNN, Svm=table_group_Svm)
+}
+
+
+find_real <- function(data_real, data_simu_train, data_simu){
+  
+  data_train <- rbind(data_real, data_simu_train)
+  metadata <- c(rep("real", nrow(data_real)), rep("simu", nrow(data_simu_train))) %>% as.factor()
+  train <- data.frame(data_train, metadata ,row.names = NULL)
+  colnames(train) <- c(paste("X",1:ncol(data_train), sep=""), "metadata")
+  
+  test <- data.frame(data_simu, row.names = NULL)
+  colnames(test) <- paste("X",1:ncol(test), sep="")
+
+  
+  forest <- randomForest(metadata~., train)
+  pred <- predict(forest, test)
+
+  iid <- which(pred=="real")
+  
+  iid
+}
+
+
+
+find_nb_sample_cluster <- function(metadata){
+  name_group <- unique(metadata)
+  sapply(name_group, function(x){sum(x==metadata)})
+}
+
+
+classificateur_group_real_simu <- function(data, metadata){
+  
+  apprent <- apprentissage_supervise(data, metadata)
+  simu <- simulation_supervise(apprent, nb_sample=10*find_nb_sample_cluster(metadata = metadata))
+  
+  s <- sample(1:nrow(simu$data), nrow(data))
+  data_simu_train <- simu$data[s, ]
+  data_simu <- simu$data[-s, ]
+  metadata_simu <- simu$metadata[-s]
+  
+  iid_real <- find_real(data_real = data, data_simu_train = data_simu_train, data_simu = data_simu)
+  
+  data_real <- data_simu[iid_real, ]
+  metadata_real <- metadata_simu[iid_real]
+
+
+  train <- data.frame(data, as.factor(metadata), row.names = NULL)
+  colnames(train) <- c(paste("X",1:ncol(data), sep=""), "metadata")
+  
+  test <- data.frame(data_real, row.names = NULL)
+  colnames(test) <- paste("X",1:ncol(test), sep="")
+  
+  
+  find_group(train=train, test = test, metadata_test =  metadata_real)
+  
 }
 
