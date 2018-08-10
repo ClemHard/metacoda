2b122b6556a7463b3e0dc303c8fe2cfa3caeeeda
LenovoX-240 <vicclem5999@live.fr>
2018-08-10 18:09:09 +0200

    graph_biplot_normale: ellipse=FALSE

--- Rscript/graph.R
+++ Rscript/graph.R
@@ -61,7 +61,7 @@ Graph_cumulative_evolution<-function(data, abscisse=1:nrow(data)){
 
 
 
-graph_biplot_normale <- function(data, metadata_group, nb_graph=1, title=NULL, legend_title="group", coord_biplot=FALSE, base_binaire=Base_binary_matrix(ncol(data))){
+graph_biplot_normale <- function(data, metadata_group, nb_graph=1, title=NULL, legend_title="group", coord_biplot=FALSE, base_binaire=Base_binary_matrix(ncol(data)), ellipse=TRUE){
   
   b_data <- 0
   
@@ -86,41 +86,30 @@ graph_biplot_normale <- function(data, metadata_group, nb_graph=1, title=NULL, l
   name_group <- nth(summarise(group_by(m,group)),1)
   
   ellipse_confiance <- list()
-  for(i in 1:nb_graph) ellipse_confiance[[i]] <- data.frame(x = numeric(), y = numeric(), group = character(), stringsAsFactors = FALSE)
+  if(ellipse==TRUE){
+    for(i in 1:nb_graph) ellipse_confiance[[i]] <- data.frame(x = numeric(), y = numeric(), group = character(), stringsAsFactors = FALSE)
   
-  for(i in name_group){
-    for(j in 1:nb_graph){
-      if(nrow((filter(m,group==i)[,j:(j+1)]))>5){
-        if(((filter(m,group==i)[,j:(j+1)]) %>% as.matrix() %>%ilr_inverse() %>% Raduis_test())$Watson<0.187){
-          temp <- as.matrix(filter(m,group==i)[,j:(j+1)])
-          el <- intervalle_confiance(temp, alpha=0.05,1,moy=apply(temp,2,mean),var=var(temp))
+     for(i in name_group){
+      for(j in 1:nb_graph){
+        if(nrow((filter(m,group==i)[,j:(j+1)]))>5){
+          if(((filter(m,group==i)[,j:(j+1)]) %>% as.matrix() %>%ilr_inverse() %>% Raduis_test())$Watson<0.187){
+           temp <- as.matrix(filter(m,group==i)[,j:(j+1)])
+            el <- intervalle_confiance(temp, alpha=0.05,1,moy=apply(temp,2,mean),var=var(temp))
           
-          ellipse_confiance[[j]] <- bind_rows(ellipse_confiance[[j]], 
+            ellipse_confiance[[j]] <- bind_rows(ellipse_confiance[[j]], 
                                               data.frame(x = el[ , 1], 
                                                          y = el[, 2], 
                                                          group = i, 
                                                          stringsAsFactors = FALSE))
           
+          }
         }
       }
     }
   }
   
-  # check_type_data <- function(data_list){
-  #   data_list[,1] <- data_list[,1] %>% as.character() %>% as.numeric()
-  #   data_list[,2] <- data_list[,2] %>% as.character() %>% as.numeric()
-  #   data_list[,3] <- data_list[,3] %>% as.character()
-  #   
-  #   data_list
-  # }
-  # 
-  # for(i in 1:length(ellipse_confiance)){
-  #   if(nrow(ellipse_confiance[[i]])!=0){
-  #     ellipse_confiance[[i]] %<>% check_type_data()
-  #   }
-  # }
   
-  create_graph <- function(data, ellipse){
+  create_graph <- function(data){
     
     graph_bc <- list()
     for(i in 1:nb_graph){
@@ -128,7 +117,7 @@ graph_biplot_normale <- function(data, metadata_group, nb_graph=1, title=NULL, l
         labs(title=title, y=paste("comp",i+1), x=paste("comp",i)) +
         scale_color_discrete(name=legend_title) + 
         theme(plot.title = element_text(hjust=0.5))
-      if(nrow(ellipse_confiance[[i]])!=0){
+      if(ellipse==TRUE && nrow(ellipse_confiance[[i]])!=0){
         data_ellipse <- ellipse_confiance[[i]]
         g <- g + geom_path(data = data_ellipse, aes(x=x, y=y, group=group, col=group))
       }
@@ -137,6 +126,6 @@ graph_biplot_normale <- function(data, metadata_group, nb_graph=1, title=NULL, l
     graph_bc
   }
   
-  create_graph(m,ellipse_confiance)
+  create_graph(m)
   
 }
